325c6baf5a08463880150f99759c664e251336a5
Parisotto <parisotto@gmail.com>
2024-12-03 08:57:28 -0300

    03/12/2024

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from datetime import datetime as dt
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import random
+import os
 
 app = Flask(__name__, template_folder ="siteciencia/templates", static_folder = "siteciencia/static")
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, "cd.sqlite")
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Task(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(200), nullable=False)
+  date_created = db.Column(db.DateTime, default=dt.now())
+  def __repr__(self):
+    return  f"Task: #{self.id}, description: {self.description}"
+
+class Frase(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  frase = db.Column(db.String(500), nullable=False)
+  autor = db.Column(db.String(50), default='Anônimo')
+  def __repr__(self):
+    return  f"Frase: #{self.id}, autor: {self.autor}, frase: {self.frase}"
+
+  
+  
 
 @app.route("/")
 def index():
